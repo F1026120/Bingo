53ee6aaa2cf935d64fadd9b288bf3f88dadaa4bc
F1026120 <mays2288@gmail.com>
2017-05-22 01:48:37 +0800

    修正演算Bug

--- BingoBoard.cs
+++ BingoBoard.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BingoBoard 
+public class BingoBoard
 {
     public int[,] m_Board = new int[5, 5];
     int[] m_LineValue = new int[12];
-	
+
     public BingoBoard()
-    {        
+    {
     }
 
     // 初始賓果盤
     public void InitBoard()
     {
         // 填值
-        int NowNum =1;
+        int NowNum = 1;
         for (int i = 0; i < 5; ++i)
             for (int j = 0; j < 5; ++j)
                 m_Board[i, j] = NowNum++;
@@ -35,44 +35,42 @@ public class BingoBoard
     public int CountLine()
     {
         int valueindex = 0;
-        int tempvalue = 0;        
+        int tempvalue = 0;
 
         //計算列值
-        for (int i = 0; i < 5; i++)                    
+        for (int i = 0; i < 5; i++)
         {
-            tempvalue = 0;        
-            for (int j = 0; j < 5; j++)            
-                tempvalue += m_Board[i,j];//
+            tempvalue = 0;
+            for (int j = 0; j < 5; j++)
+                tempvalue += m_Board[i, j];//
             m_LineValue[valueindex++] = tempvalue;
         }
 
         //計算行值
         for (int j = 0; j < 5; j++)
         {
-            tempvalue = 0;        
-            for (int i = 0; i < 5; i++)                                        
-                tempvalue += m_Board[i,j];
-            m_LineValue[valueindex++] = tempvalue;
-        }       
-        
-        // TODO:左斜,右斜
-
-        for (int i =  0;i < 5; i++)
-        {
             tempvalue = 0;
+            for (int i = 0; i < 5; i++)
+                tempvalue += m_Board[i, j];
+            m_LineValue[valueindex++] = tempvalue;
+        }
+
+        // TODO:左斜,右斜
+        tempvalue = 0;
+        for (int i = 0; i < 5; i++)
+        {
             tempvalue += m_Board[i, i];
         }
         m_LineValue[valueindex++] = tempvalue;
-
-        for(int i = 4; i >= 0; i--)
+        tempvalue = 0;
+        for (int i = 4; i >= 0; i--)
         {
-            tempvalue = 0;
             tempvalue += m_Board[i, 4 - i];
         }
 
         m_LineValue[valueindex++] = tempvalue;
 
-                                
+
         //計算連線行數
         int lines = 0;
         for (int i = 0; i < 12; i++)
@@ -85,12 +83,12 @@ public class BingoBoard
     public void SetNumber(int Value)
     {
         for (int i = 0; i < 5; i++)
-            for (int j = 0; j < 5; j++)            
+            for (int j = 0; j < 5; j++)
                 if (m_Board[i, j] == Value)
                 {
-                    m_Board[i, j] = 0;                    
+                    m_Board[i, j] = 0;
                     return;
-                }        
+                }
     }
 
 
